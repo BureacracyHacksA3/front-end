d8aca08c52db3c723d919e028857e9c30f4ed1ab
Ionela Alexandra <63660313+ionero12@users.noreply.github.com>
2023-05-10 23:42:55 +0300

    added buttons

--- front_all_pages/lib/dropdown+documents_page/main.dart
+++ front_all_pages/lib/dropdown+documents_page/main.dart
@@ -1,6 +1,6 @@
 import './models/document.dart';
-import './task_service.dart';
-import './documents_service.dart';
+import 'services/task_service.dart';
+import 'services/documents_service.dart';
 import 'package:flutter/material.dart';
 import './buttomNavigationBar.dart';
 import 'package:adaptive_navbar/adaptive_navbar.dart';
@@ -87,55 +87,65 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
           child: Container(
+        //toata pagina
         color: const Color(0xff293441),
         child: Center(
           child: Container(
+            //width: 350,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(40),
               color: const Color(0xff293441),
             ),
-            width: 850, // setați lățimea dorită
+            //width: 350, // setați lățimea dorită
             //height: 400, // setați înălțimea dorită
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                DropdownButtonFormField(
-                  dropdownColor: const Color(0xff896F4E),
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 16.0, vertical: 8.0),
-                    filled: true,
-                    fillColor: const Color(0xff896F4E),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide.none,
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: DropdownButtonFormField(
+                    isExpanded: true,
+                    //icon: Container(width: 0, height: 0),
+                    dropdownColor: const Color(0xff896F4E),
+                    decoration: InputDecoration(
+                      //contentPadding: const EdgeInsets.symmetric(
+                      //   horizontal: 16.0, vertical: 8.0),
+                      filled: true,
+                      fillColor: const Color(0xff896F4E),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        borderSide: BorderSide.none,
+                      ),
+                      hintStyle: const TextStyle(color: Colors.white),
+                      hintText: 'Selecteaza document',
                     ),
-                    hintStyle: const TextStyle(color: Colors.white),
-                    hintText: 'Selecteaza document',
-                    hintMaxLines: 3,
-                  ),
-                  value: _selectedOption,
-                  onChanged: (newValue) {
-                    setState(() {
-                      _selectedOption = newValue.toString();
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              MyOtherPage(selectedOption: _selectedOption!),
+                    value: _selectedOption,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedOption = newValue.toString();
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                MyOtherPage(selectedOption: _selectedOption!),
+                          ),
+                        );
+                      });
+                    },
+                    items: _options.map((option) {
+                      return DropdownMenuItem(
+                        value: option,
+                        child: Text(
+                          option,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 1,
                         ),
                       );
-                    });
-                  },
-                  items: _options.map((option) {
-                    return DropdownMenuItem(
-                      value: option,
-                      child: Text(
-                        option,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    );
-                  }).toList(),
+                    }).toList(),
+                  ),
                 ),
               ],
             ),
@@ -177,6 +187,7 @@ class _MyOtherPageState extends State<MyOtherPage> {
   @override
   Widget build(BuildContext context) {
     final sw = MediaQuery.of(context).size.width;
+    bool isButtonVisible=false;
 
     return Scaffold(
       backgroundColor: const Color(0xff293441),
@@ -217,113 +228,141 @@ class _MyOtherPageState extends State<MyOtherPage> {
       ),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Acte necesare pentru eliberare ${widget.selectedOption}',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontFamily: "Inria Serif",
-                      fontSize: 30,
-                      color: Colors.white),
+          child: SizedBox(
+            height: 1200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Acte necesare pentru eliberare ${widget.selectedOption}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                        fontFamily: "Inria Serif",
+                        fontSize: 30,
+                        color: Colors.white),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-              child: Container(
-                padding: EdgeInsets.all(20.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(40),
-                  color: const Color.fromARGB(255, 137, 111, 78),
-                ),
-                //height: 700,
-                //width: 600,
-                child: Column(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: List.generate(
-                        _documents?.length ?? 0,
-                        (index) => Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          //+
-                          //EdgeInsets.only(left: 8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${index + 1}. ${_documents?[index].name} ',
-                                style: const TextStyle(
-                                  fontFamily: 'Inria Serif',
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                ),
-                                maxLines: 3,
-                              ),
-                              Text(
-                                'Price: ${_documents?[index].price}',
-                                style: const TextStyle(
-                                  fontFamily: 'Inria Serif',
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                ),
-                              ),
-                              Text(
-                                'Link: ${_documents?[index].path}',
-                                style: const TextStyle(
-                                  fontFamily: 'Inria Serif',
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(40),
+                      color: const Color.fromARGB(255, 137, 111, 78),
                     ),
-                    Image.asset('images/schema.png'),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                      // Set padding from content above
-                      child: ElevatedButton(
-                        onPressed: () {
-                          // Button action goes here
-                        },
-                        child: Text('Incepe operatiunea',
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                            )),
-                        style: ButtonStyle(
-                          // Customize background color
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Color(0xff293441)),
+                    //height: 1000, //setarea asta s-ar putea sa ti faca probleme
+                    width: 600,
+                    child: Column(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: List.generate(
+                            _documents?.length ?? 0,
+                            (index) => Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(vertical: 8.0),
+                              //+
+                              //EdgeInsets.only(left: 8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '${index + 1}. ${_documents?[index].name} ( Price: ${_documents?[index].price} )',
+                                    style: const TextStyle(
+                                      fontFamily: 'Inria Serif',
+                                      fontSize: 20,
+                                      color: Colors.white,
+                                    ),
+                                    maxLines: 3,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10),
+                                    child: Row(
+                                      children: [
+                                        ElevatedButton(
+                                          onPressed: () {
+                                            // Button 1 onPressed callback
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(40),
+                                            ),
+                                            primary: const Color(0xFF101C2B),
+                                          ),
+                                          child: const Text('Upload'),
+                                        ),
+                                        const SizedBox(width: 16), // Add some spacing between the buttons
+                                        Visibility(
+                                          visible: isButtonVisible, // Set the visibility condition
+                                          maintainSize: true,
+                                          maintainAnimation: true,
+                                          maintainState: true,
+                                          child: ElevatedButton(
+                                            onPressed: () {
+                                              // Button 2 onPressed callback
+                                            },
+                                            style: ElevatedButton.styleFrom(
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(40),
+                                              ),
+                                              primary: const Color(0xFF101C2B),
+                                            ),
+                                            child: Text('Button 2'),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  )
 
-                          // Customize button padding
-                          padding: MaterialStateProperty.all<EdgeInsets>(
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                          ),
-
-                          // Customize border
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              side: BorderSide(color: Color(0xFF101C2B)),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
+                        Image.asset('images/schema.png'),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          // Set padding from content above
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Button action goes here
+                            },
+                            style: ButtonStyle(
+                              // Customize background color
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  const Color(0xff293441)),
+
+                              // Customize button padding
+                              padding: MaterialStateProperty.all<EdgeInsets>(
+                                const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 20),
+                              ),
+
+                              // Customize border
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  side: const BorderSide(color: Color(0xFF101C2B)),
+                                ),
+                              ),
+                            ),
+                            child: const Text('Incepe operatiunea',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ),
-              ),
-              Image.asset('images/orase.png'),
-            ],
+                Image.asset('images/orase.png'),
+              ],
+            ),
           ),
         ),
       ),
